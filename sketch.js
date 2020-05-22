bb3689485a4b93a953c82c38053049c244235638
Jade570 <44695194+Jade570@users.noreply.github.com>
2020-05-23 00:23:03 +0900

    Change x,y,z coordinate ^y |---->x, forward z

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ object = loadJSON("object.json");
 function setup(){
   cam_x = 0;
   cam_y = 0;
-  cam_z = windowHeight/2/ tan(PI*30.0 / 180.0);
+  cam_z = -(windowHeight/2/ tan(PI*30.0 / 180.0));
   cam_dx = 0;
   cam_dy = 0;
   cam_dz = 0;
@@ -29,26 +29,44 @@ function setup(){
 }
 
 function draw(){
-     a = new People(object[0], col);
+  a = new People(object[0], col);
   background(255);
+
   updateCamCenter();
-  camera(cam_x, cam_y, cam_z,cam_cx, cam_cy, cam_cz,0,1,0);
+  camera(cam_x, cam_y, cam_z,cam_cx, cam_cy, cam_cz,0,-1,0);
   pan += movedX/128;
   tilt -= movedY/128;
-  updateCamCenter();
+
+  //updateCamCenter();
   fill(0,0,255);
 //  plane(1000, 1000);
 
   push();
   fill(0);
-   a.render();
+  // a.render();
   pop();
 
     //translate(obj_x, obj_y, obj_z);
     //translate(obj_x1, obj_y1, obj_z1);
-    translate(0,0,cam_z*2);
+    //translate(0,0,cam_z*2);
+    push();
+    translate(0,50, obj_z);
+    fill(255,0,0);
+    box();
+    pop();
+
+    push();
+    translate(0,0, obj_z);
     fill(0,255,0);
     box();
+    pop();
+
+    push();
+    translate(0,-50, obj_z);
+    fill(0,0,255);
+    box();
+    pop();
+
 
 
   //console.log(object[0]);
@@ -61,20 +79,18 @@ function mouseClicked(){
 
 
 function updateCamCenter(){
-  cam_dz = 1;
-  cam_dx = -sin(pan);
-  cam_dy = -sin(tilt);
+  cam_dz = cos(pan)*cos(tilt);
+  cam_dx = sin(pan);
+  cam_dy = sin(tilt);
 
   // compute scene center position
   cam_cx = cam_x + cam_dx;
   cam_cy = cam_y + cam_dy;
   cam_cz = cam_z + cam_dz;
 
-  obj_x = cam_x + 3*cam_dx;
-  obj_y = cam_y + 3*cam_dy;
-  obj_z = cam_z +cam_dz;
+  obj_x = cam_x + cam_dx*2;
+  obj_y = cam_y + cam_dy*2;
+  obj_z = cam_dz*2;
 
-  obj_x1 = cam_x - cam_dx/64;
-  obj_y1 = cam_y - cam_dy/64;
-  obj_z1 = cam_z + cam_dz;
+
 }
