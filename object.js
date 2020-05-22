9bfeedf89168166a9263a7d79e9451bf09b8309c
Jade570 <44695194+Jade570@users.noreply.github.com>
2020-05-23 01:37:40 +0900

    Update object structure - add color

--- object.js
+++ object.js
@@ -1,10 +1,10 @@
 class People{
 
-  constructor(jsonobj, color){
+  constructor(jsonobj){
     this.time = jsonobj.time;
     this.pos = createVector(jsonobj.x,jsonobj.y,jsonobj.z);
-    this.pos = createVector(cam_x+(jsonobj.rad)*sin(jsonobj.pan) , cam_y+(jsonobj.rad)*sin(jsonobj.tilt) , cam_z+(jsonobj.rad)*cos(jsonobj.pan)*cos(jsonobj.tilt));
-    this.color = color;
+    this.pos = createVector(cam_x+(jsonobj.rad)*sin(radians(jsonobj.pan)) , cam_y+(jsonobj.rad)*sin(radians(jsonobj.tilt)) , cam_z+(jsonobj.rad)*cos(radians(jsonobj.pan))*cos(radians(jsonobj.tilt)));
+    this.color = color(jsonobj.r, jsonobj.g, jsonobj.b);
 
     //we need to compare cam(pan&tilt) w/ jsonobj.pan&tilt.
   }
