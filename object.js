c760c33feb983e8a4c14f310ce12e2208d024379
Jade570 <44695194+Jade570@users.noreply.github.com>
2020-05-23 01:26:56 +0900

    Change object structure

--- object.js
+++ object.js
@@ -3,7 +3,10 @@ class People{
   constructor(jsonobj, color){
     this.time = jsonobj.time;
     this.pos = createVector(jsonobj.x,jsonobj.y,jsonobj.z);
+    this.pos = createVector(cam_x+(jsonobj.rad)*sin(jsonobj.pan) , cam_y+(jsonobj.rad)*sin(jsonobj.tilt) , cam_z+(jsonobj.rad)*cos(jsonobj.pan)*cos(jsonobj.tilt));
     this.color = color;
+
+    //we need to compare cam(pan&tilt) w/ jsonobj.pan&tilt.
   }
 
   render(){
